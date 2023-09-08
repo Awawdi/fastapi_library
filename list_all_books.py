f78305b988f43b86206c3833352a07d3d26ca608
Awawdi <orsan.awawdi@gmail.com>
2023-09-08 19:54:45 +0300

    added authors and books

--- list_all_books.py
+++ list_all_books.py
@@ -6,11 +6,11 @@ def load_file():
      with open(LIBRARY_JSON_FILE, "r") as dummy_file:
          return json.load(dummy_file)
 
-def read_item_by_id(item_id:str):
+def read_all_authors():
     with open(LIBRARY_JSON_FILE, "r") as dummy_file:
         all_books_list = json.load(dummy_file)["books"]
-        res = [d for d in all_books_list if d.get("isbn",None) == item_id]
-        return res[0]
+        result = [d.get("author",None) for d in all_books_list]
+        return set(result)
 
 if __name__ == "__main__":
-    print(read_item_by_id("9781491943533"))
+    print(read_all_authors())
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import json
 from fastapi import FastAPI
 from pydantic import BaseModel
-
-
-class Item(BaseModel):
-    name: str
-    price: float
+import requests
 
 LIBRARY_JSON_FILE = "library.json"
 
@@ -15,16 +11,30 @@ app = FastAPI()
 async def root():
     return load_file(LIBRARY_JSON_FILE)
 
-@app.get("/items/{item_id}")
+@app.get("/books")
+async def read_all_books():
+    with open(LIBRARY_JSON_FILE, "r") as dummy_file:
+       all_books_list = json.load(dummy_file)["books"]
+       result = [d.get("title",None) for d in all_books_list]
+       return set(result)
+
+@app.get("/books/{item_id}")
 async def read_item(item_id):
     with open(LIBRARY_JSON_FILE, "r") as dummy_file:
         all_books_list = json.load(dummy_file)["books"]
         res = [d for d in all_books_list if d.get("isbn",None) == item_id]
         return res[0]
 
+@app.get("/authors")
+async def read_all_authors():
+    with open(LIBRARY_JSON_FILE, "r") as dummy_file:
+       all_books_list = json.load(dummy_file)["books"]
+       result = [d.get("author",None) for d in all_books_list]
+       return set(result)
+
 
 def load_file(selected_file):
      with open(selected_file, "r") as dummy_file:
-         return json.load(dummy_file)
+        return json.load(dummy_file)
      
     
