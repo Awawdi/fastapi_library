7fb99b85c9fda0798402eb39b33daafc81b4be42
Awawdi <orsan.awawdi@gmail.com>
2023-09-09 12:56:40 +0300

    updated main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
-from fastapi import FastAPI
-from pydantic import BaseModel
-import requests
+from fastapi import FastAPI, Request
+
 
 LIBRARY_JSON_FILE = "library.json"
 
@@ -9,32 +8,30 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return load_file(LIBRARY_JSON_FILE)
+    return load_file()
 
 @app.get("/books")
-async def read_all_books():
-    with open(LIBRARY_JSON_FILE, "r") as dummy_file:
-       all_books_list = json.load(dummy_file)["books"]
-       result = [d.get("title",None) for d in all_books_list]
-       return set(result)
+async def read_all_books(request:Request):
+    all_books_list = load_file()
+    result = [d.get("title",None) for d in all_books_list]
+    return f"Listing all books. Request coming from {request.url._url}, response: {set(result)}"
 
 @app.get("/books/{item_id}")
 async def read_item(item_id):
-    with open(LIBRARY_JSON_FILE, "r") as dummy_file:
-        all_books_list = json.load(dummy_file)["books"]
-        res = [d for d in all_books_list if d.get("isbn",None) == item_id]
-        return res[0]
+    all_books_list = load_file()
+    res = [d for d in all_books_list if d.get("isbn",None) == item_id]
+    return res[0]
 
 @app.get("/authors")
 async def read_all_authors():
-    with open(LIBRARY_JSON_FILE, "r") as dummy_file:
-       all_books_list = json.load(dummy_file)["books"]
-       result = [d.get("author",None) for d in all_books_list]
-       return set(result)
+    all_books_list = load_file()
+    result = [d.get("author",None) for d in all_books_list]
+    return set(result)
 
 
-def load_file(selected_file):
+def load_file(selected_file : str = LIBRARY_JSON_FILE):
      with open(selected_file, "r") as dummy_file:
-        return json.load(dummy_file)
+        temp = json.dumps(dummy_file)
+        
      
     
