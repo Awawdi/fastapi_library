b39b11e9f9c6b6f7a96b5f2c7baf072b04e102bc
Orsan Awawdi <orsan.awawdi@gmail.com>
2023-09-10 22:25:23 +0300

    Create main-authors.py

---
+++ main-authors.py
@@ -0,0 +1,25 @@
+import json
+from fastapi import FastAPI, Request
+
+
+LIBRARY_JSON_FILE = "library.json"
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return load_file()
+
+@app.get("/authors")
+async def read_all_authors():
+    all_books_list = load_file()
+    result = [d.get("author",None) for d in all_books_list]
+    return set(result)
+
+
+def load_file(selected_file : str = LIBRARY_JSON_FILE):
+     with open(selected_file, "r") as dummy_file:
+        return json.load(dummy_file)["books"]
+        
+     
+    
