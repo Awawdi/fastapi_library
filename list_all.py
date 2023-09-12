8096efaf6af6a81e5f72015b3b7202eced75d05e
Awawdi <orsan.awawdi@gmail.com>
2023-09-12 11:27:15 +0300

    fixed path

--- list_all.py
+++ list_all.py
@@ -3,18 +3,11 @@ from fastapi import FastAPI
 from pathlib import Path
 import os
 
-REPO_ABSPATH = Path(os.path.abspath(__file__)).parent.parent
+REPO_ABSPATH = Path(os.path.abspath(__file__)).parent
 
 LIBRARY_JSON_FILE = REPO_ABSPATH / "library.json"
 
-app = FastAPI()
-
-@app.get("/")
-async def root():
-    return load_file()
-
-@app.get("/authors")
-async def read_all_authors():
+def read_all_authors():
     all_books_list = load_file()
     result = [d.get("author",None) for d in all_books_list]
     return set(result)
@@ -23,6 +16,8 @@ async def read_all_authors():
 def load_file(selected_file : str = LIBRARY_JSON_FILE):
      with open(selected_file, "r") as dummy_file:
         return json.load(dummy_file)["books"]
-        
+
+if __name__ == "__main__":
+    print (read_all_authors())
      
     
