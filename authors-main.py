2b1419937d0d9a6c36d6b4f5fd93b185be58ad07
Awawdi <orsan.awawdi@gmail.com>
2023-09-13 10:54:16 +0300

    updated with ingress

--- authors-main.py
+++ authors-main.py
@@ -15,8 +15,17 @@ async def root():
 async def read_all_authors():
     all_books_list = load_file()
     result = [d.get("author",None) for d in all_books_list]
+    if not result:
+        raise Exception("No author was found!.")
     return set(result)
 
+@app.get("/authors/{item_id}")
+async def read_item(item_id):
+    all_books_list = load_file()
+    result = [d for d in all_books_list if d.get("author",None) == item_id]
+    if not result:
+        raise Exception(f"Author {item_id} was found!.")
+    return result[0]
 
 def load_file(selected_file : str = LIBRARY_JSON_FILE):
      with open(selected_file, "r") as dummy_file:
--- books.Dockerfile
+++ books.Dockerfile
@@ -3,9 +3,11 @@ FROM python:3.11.1-slim
 WORKDIR /app
 
 # install dependencies
-COPY ./requirements.txt ./
+COPY requirements.txt .
 RUN pip install -r requirements.txt
 
 COPY books-main.py /app
 COPY library.json /app
-CMD ["uvicorn", "books-main:app", "--port", "7000", "--reload"]
+EXPOSE 8000
+ENV PYTHONPATH=/app
+CMD ["uvicorn", "books-main:app", "--host", "0.0.0.0", "--port", "8000"]
